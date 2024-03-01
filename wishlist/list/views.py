be95a6b61f598ca3d1fb12d480d46ff2a8cd05d2
Vicky <nakamanoaki@gmail.com>
2024-02-29 20:39:13 -0500

    First commit

---
+++ wishlist/list/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def test(request):
+    return HttpResponse("Hello world!")
