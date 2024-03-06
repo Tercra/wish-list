449b37dd54bfcbda5561d404b8163b0afd9e20c8
Vicky <nakamanoaki@gmail.com>
2024-03-05 19:58:18 -0500

    Registering Users is working and the page for it is set up (log in is setup but not working yet)

--- wishlist/list/urls.py
+++ wishlist/list/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name="test"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
 ]
--- wishlist/list/views.py
+++ wishlist/list/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def test(request):
     return HttpResponse("Hello world!")
+
+def login(request):
+    if(request.method == "POST"):
+        pass
+    template = loader.get_template('loginRegister.html')
+    context = {"page" : "login"}
+    return HttpResponse(template.render(context=context, request=request))
+
+def register(request):
+    if(request.method == "POST"):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            for err in form.errors:
+                messages.error(request, form.errors[err])
+    else:
+        form = UserCreationForm()
+
+    template = loader.get_template('loginRegister.html')
+    context = {"page" : "register", "form" : form}
+    return HttpResponse(template.render(context=context, request=request))
